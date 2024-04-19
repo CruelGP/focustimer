d95419cce343dd84c7e08f7971be818a91b58d53
Gustavo Pamponet <106318703+CruelGP@users.noreply.github.com>
2024-04-19 08:50:47 -0300

    up

---
+++ src/FocusTimer/actions.js
@@ -0,0 +1,18 @@
+import state from './state.js'
+
+export function toggleRunning(){
+    state.isRunning = document.documentElement.classList.toggle('running')
+}
+
+export function reset(){
+    state.isRunning = false
+    document.documentElements.classList.remove('running')
+}
+
+export function set(){
+    
+}
+
+export function toggleMusic(){
+    stete.isMute = document.documentElement.classList.toggle('music-on')
+}
